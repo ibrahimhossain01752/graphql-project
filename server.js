20278e7e08dccf76613db8b78d690ec53896207f
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-02-20 10:58:33 +0600

    graph data

--- server.js
+++ server.js
@@ -27,25 +27,46 @@
 // app.listen(5000., () => console.log('server is running'))
 
 
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql');
+const { resolve } = require('path');
+
+
+const books = [
+  {id:1, name: 'Harry Potter and the number the Chamber of Secrets', authorId:1},
+  {id:2, name: 'Harry Potter and the Prisoner  of Azkaban', authorId:1},
+  {id:3, name: 'Harry Potter and the Goblet of Fire', authorId:1},
+  {id:4, name: 'The Fellowship of the Ring', authorId:2},
+  {id:5, name: 'The Two Towers', authorId:2},
+  {id:6, name: 'The Return of the King', authorId:2},
+  {id:7, name: 'The way of the Shadow', authorId:3},
+  {id:8, name: 'Beyond the Shadow', authorId:3},
+
+]
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+const schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
 // The root provides a resolver function for each API endpoint
-var root = {
+const root = {
+  name : 'Quety',
+  description:'Root Query',
   hello: () => {
-    return 'Hello world!';
+    books:{
+      type: BookType,
+      description:'List of books',
+      
+    }
+    // return 'Helloworld!';
   },
 };
 
-var app = express();
+const app = express();
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
