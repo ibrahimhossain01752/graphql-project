133bf46e5604adb858f9a64f22a2df68f742fe79
ibrahimhossain01752 <ibrahimhossain01752@gmail.com>
2023-02-21 01:12:19 +0600

    begginer level finish

--- server.js
+++ server.js
@@ -2,27 +2,51 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 const { resolve } = require('path');
+const axios = require('axios');
 
 
-// const books = [
-//   {id:1, name: 'Harry Potter and the number the Chamber of Secrets', authorId:1},
-//   {id:2, name: 'Harry Potter and the Prisoner  of Azkaban', authorId:1},
-//   {id:3, name: 'Harry Potter and the Goblet of Fire', authorId:1},
-//   {id:4, name: 'The Fellowship of the Ring', authorId:2},
-//   {id:5, name: 'The Two Towers', authorId:2},
-//   {id:6, name: 'The Return of the King', authorId:2},
-//   {id:7, name: 'The way of the Shadow', authorId:3},
-//   {id:8, name: 'Beyond the Shadow', authorId:3},
-
-// ]
+let message = "This is a message"
 
 // Construct a schema, using GraphQL schema language
 const schema = buildSchema(`
-  type Query {
+
+type Post {
+  userId: Int
+  id: Int
+  title: String
+  body: String
+}
+
+type User {
+  name: String
+  age: Int
+  college: String
+}
+
+type Query {
     hello: String
     welcomeMessage(name: String, dayofweek: String!): String
+    getUser: User
+    getUsers:[User]
+    getPostFromExternalAPI: [Post]
+    message: String
   }
+
+  input UserInput {
+    name: String!
+    age: Int!
+    college: String!
+  }
+
+  type Mutation {
+    setMessage(newMessage: String): String
+    createUser(user: UserInput) : User
+  }
+
 `);
+//createUser(name: String!, age: Int!, college: String!) : User
+
+
 
 // The root provides a resolver function for each API endpoint
 const root = {
@@ -32,7 +56,55 @@ const root = {
  welcomeMessage:(args) => {
   console.log(args);
   return `Hey ${args.name} hows life, today is ${args.dayofweek}`;
- }
+ },
+ getUser: () => {
+  const user = {
+    name: 'Ibrahim Hossain',
+    age: 21,
+    college:'AHC'
+  }
+  return user;
+ },
+ getUsers : () => {
+ const users = [
+  {
+    name: 'Homyra',
+    age: 16,
+    college:'Girls'
+  },
+  {
+    name: 'Shimaa',
+    age: 26,
+    college:'dhaka'
+  }
+ ]
+ return users;
+ },
+//  getPostFromExternalAPI: () => {
+//    return axios.get('https://jsonplaceholder.typicode.com/posts')
+//     .then(result => result.data);
+//  }
+
+getPostFromExternalAPI: async () => {
+  const result = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  return result.data;
+},
+
+setMessage: ({newMessage}) => {
+  message = newMessage;
+  return message
+},
+
+message: () => message,
+createUser: (args) => {
+  console.log(args)
+  //{name, age, college}
+    //create a new user inside or external api or even firestore
+    return args.user;
+    //{name, age, college}
+}
+
+
 };
 
 const app = express();
